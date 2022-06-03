da8048e7efc50cb2f594857bfd717ad4023dc2a2
Maxime VILAR <maxime.vilar@gmail.com>
2022-06-03 15:40:27 +0200

    tkt c un test

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+$value = $_GET['value'] ?? '';
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://api.tvmaze.com/search/shows?q=fast',
+    CURLOPT_URL => 'https://api.tvmaze.com/search/shows?q=' . $value,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
@@ -23,3 +25,50 @@ foreach ($movies as $movie) {
 
     echo $movie->show->name . "<br>";
 }
+
+function search(string $needed, array $data): array
+{
+    foreach ($data as $movie) {
+        if (strpos($movie->show->name, $needed) !== false) {
+            $result[] = $movie->show->name;
+        }
+    }
+    return $result;
+}
+
+?>
+
+<p>Search</p>
+
+<input type="text" id="textValue">
+<input type="button" value="Valider" id="validButton">
+
+<p class="validSearch"></p>
+
+
+
+<script type="text/javascript">
+
+window.addEventListener('load', function() {
+    console.log('All assets are loaded')
+
+    document.getElementById('validButton').addEventListener('click', function() {
+        let value = document.getElementById('textValue').value
+
+        fetch('https://api.tvmaze.com/search/shows?q=' + value)
+        .then(function(response) {
+            console.log(response);
+            // (response.body).for(element => {
+            // });
+            for (const [key, value] of Object.entries(response.body)) {
+                console.log(`${key}: ${value}`);
+                console.log('toto')
+                // console.log(element.show.name)
+            }
+        })
+        
+    })
+
+})
+
+</script>
